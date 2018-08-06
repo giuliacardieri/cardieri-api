83a0037a7741550856d15c8703c7557240fbd925
Giulia Cardieri <giulia.cardieri@gmail.com>
2018-08-06 01:31:35 -0300

    fix app headers

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ app.use(function(req, res, next){
 	next();
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // app.all('/*', function(req, res, next) {
 //   // CORS headers
 //   res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
