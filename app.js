96ce495765201f537bbdeec26feb7711237e0846
Giulia Cardieri <giulia.cardieri@gmail.com>
2018-08-06 01:23:23 -0300

    test core

--- app.js
+++ app.js
@@ -34,19 +34,19 @@ app.use(function(req, res, next){
 	next();
 });
 
-app.all('/*', function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET');
+// app.all('/*', function(req, res, next) {
+//   // CORS headers
+//   res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+//   res.header('Access-Control-Allow-Methods', 'GET');
   
-  // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+//   // Set custom headers for CORS
+//   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+//   if (req.method == 'OPTIONS') {
+//     res.status(200).end();
+//   } else {
+//     next();
+//   }
+// });
 
 app.use('/', IndexRouter);
 app.use('/api/empreendimentos', EmpreendimentosRouter);
